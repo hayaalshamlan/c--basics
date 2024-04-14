8cff587db6ca45d20244a3afa0f5d454c49d3864
83336 <halshamlan@yahoo.com>
2024-04-14 10:41:11 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+int id = 8700;
+double gpa = 3;
+string name = "haya";
+bool isTheIdActive = false;
+
+double result = Convert.ToDouble(id);
+
+Console.WriteLine(id);
+Console.WriteLine(result);
+Console.WriteLine(gpa);
+Console.WriteLine(name);
+Console.WriteLine(isTheIdActive);
+
+
+string studentName = "haya";
+int studentGrade = 4;
+string studentFavSubject = "english";
+
+double width = 5.5;
+double height = 3.2;
+double size = width * height;
+
+Console.WriteLine(studentName);
+Console.WriteLine(studentGrade);
+Console.WriteLine(studentFavSubject);
+Console.WriteLine(size);
+
+
